ccac48462a8833d972d774dc950cce0cfa1a87ff
Paulo Afonso Cardoso Franco - Turma 36 <paulo_afonso2006@hotmail.com>
2023-09-14 16:42:11 -0300

    estilizações botões e custom selected

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
+const estilizarSelecionado = (fees) => {
+  const custom = document.getElementById('input-tip-fee');
+  for (const fee of fees) {
+    if (fee.classList.contains('selected')) {
+      fee.style.backgroundColor = 'cyan';
+      fee.style.color = 'black';
+    }
+    else {
+      fee.style.backgroundColor = 'rgb(0, 73, 77)';
+      fee.style.color = 'rgb(244, 250, 250)';
+    }
+  }
+  if (!custom.classList.contains('selected')) {
+    custom.style.backgroundColor = 'white';
+    custom.style.color= 'black';
+  }
+}
+
+const selecionar = (event) => {
+  const target = event.target;
+  if (!target.classList.contains('fee')) { return; }
+  const fees = document.getElementsByClassName('fee');
+  for (const fee of fees) {
+    if (fee.classList.contains('selected')) fee.classList.remove('selected');
+  }
+  target.classList.add('selected');
+
+  estilizarSelecionado(fees);
+}
+
 const paginaCarregada = () => {
+  const feeGroup = document.getElementById('button-group');
+  feeGroup.addEventListener('click', selecionar);
 }
 
 window.addEventListener('load', paginaCarregada);
