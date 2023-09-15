8ef09d8fa4ab8e4fc1edd37a2f6b5afb7919bab5
Paulo Afonso Cardoso Franco - Turma 36 <paulo_afonso2006@hotmail.com>
2023-09-15 15:58:45 -0300

    REM: Console.log() removido, estilizarSelecionado removido. ADD:  CSS .selected adicionado. EDIT: calcular() - selecionado recebe custom zerado caso não tenha valor selecionado.

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const estilizarSelecionado = (fees) => {
-  const custom = document.getElementById('input-tip-fee');
-  for (const fee of fees) {
-    if (fee.classList.contains('selected')) {
-      fee.style.backgroundColor = 'cyan';
-      fee.style.color = 'black';
-    }
-    else {
-      fee.style.backgroundColor = 'rgb(0, 73, 77)';
-      fee.style.color = 'rgb(244, 250, 250)';
-    }
-  }
-  if (!custom.classList.contains('selected')) {
-    custom.style.backgroundColor = 'white';
-    custom.style.color = 'black';
-  }
-};
-
 const removerSelected = (fees) => {
   for (const fee of fees) {
     if (fee.classList.contains('selected')) fee.classList.remove('selected');
@@ -27,11 +9,11 @@ const calcular = () => {
   const inputPeople = document.getElementById('input-people-number');
   const labelTipAmountByPerson = document.getElementById('label-tip-by-person');
   const labelTotalByPerson = document.getElementById('label-total-by-person');
-  const selecionado = document.getElementsByClassName('selected');
+  let selecionado = document.getElementsByClassName('selected');
   if (selecionado.length === 0) {
     labelTipAmountByPerson.innerText = '$0.00';
     labelTotalByPerson.innerText = '$0.00';
-    return;
+    selecionado = [document.getElementById('input-tip-fee')];
   }
   let tipAmount;
   if (selecionado[0].id === 'input-tip-fee') {
@@ -39,9 +21,7 @@ const calcular = () => {
   } else {
     tipAmount = Number(inputBill.value) * Number(selecionado[0].innerText.replace('%', '')) / 100;
   }
-  console.log(tipAmount);
   const total = Number(inputBill.value) + Number(tipAmount);
-  console.log(tipAmount);
   const tipAmountByPerson = Number(tipAmount) / Number(inputPeople.value);
   const totalByPerson = Number(total) / Number(inputPeople.value);
   labelTipAmountByPerson.innerText = '$' + tipAmountByPerson.toFixed(2);
@@ -51,7 +31,6 @@ const calcular = () => {
 const resetar = (event) => {
   const fees = document.getElementsByClassName('fee');
   removerSelected(fees);
-  estilizarSelecionado(fees);
   calcular();
 };
 
@@ -61,7 +40,6 @@ const selecionar = (event) => {
   const fees = document.getElementsByClassName('fee');
   removerSelected(fees);
   target.classList.add('selected');
-  estilizarSelecionado(fees);
   calcular();
 };
 
